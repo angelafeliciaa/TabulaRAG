2c744bda7c65c5bb2cf56bed792e9c3e8cf9bffa
msamanthaf <mariesamantha.f@gmail.com>
2026-04-06 16:44:32 -0700

    Folder status

--- frontend/src/pages/Upload.tsx
+++ frontend/src/pages/Upload.tsx
@@ -2080,14 +2080,12 @@ export default function Upload({ homeControls = null }: UploadProps) {
               title="Folders"
               aria-expanded={folderPanelOpen}
             >
-              <svg
-                viewBox="0 0 24 24"
-                role="presentation"
-                className="sort-toggle-icon"
+              <img
+                src={folderIcon}
+                alt=""
                 aria-hidden="true"
-              >
-                <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z" />
-              </svg>
+                className="sort-toggle-icon folder-panel-toggle__icon"
+              />
               <span className="sort-toggle-text">Folders</span>
             </button>
           </div>
